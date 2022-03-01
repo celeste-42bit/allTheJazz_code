1e726f837885d12c23c22f177a15411847c741de
jasha-gruener <studi.jasha.gruener@gmail.com>
2022-03-01 11:06:32 +0100

    Updated from editor

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from choiceeng import *
-from fileops import display
-
+# --------------------------------------------------------------------------------------
 # This is the interactive scripting language "RPGScript" for Python 3
 # CC-Celeste (Creative Commons 2022)
 # Copyright 2022 © Celeste aka. 73chn0m4nc3r
 # Sourcecode is available on GitHub https://www.github.com/celeste-42bit/allTheJazz
+# --------------------------------------------------------------------------------------
 
+from choiceeng import *
+from fileops import display
 
 # --------------------DEFINE START-POINT----------------------
 # Just enter the filename of your first text file :)
---
+++ scripts.py
@@ -0,0 +1,11 @@
+def ch0br0():  # chapter 0 branch 0
+    print("I am a script")
+
+
+def ch0br1l():  # chapter 0 branch 1 option "left"
+    print("I am the next script after choice 1")
+
+
+def ch0br1r():  # chapter 0 branch 1 option "right"
+    print("I am the other choice of branch 1")
+
