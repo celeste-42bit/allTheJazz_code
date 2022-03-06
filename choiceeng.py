bae2329aba3546c6b9821967b6450882567acee4
celeste-42bit <studi.jasha.gruener@gmail.com>
2022-03-06 13:28:21 +0100

    Updated: choiceeng.py

--- choiceeng.py
+++ choiceeng.py
@@ -1,125 +1,125 @@
-def mkch2(redA, redB):
-    userinput = str(input("ENTER: "))
-    while userinput != "A" and userinput != "B" and userinput != "a" and userinput != "b":
+def mkch2(script0, script1):
+    user_input = str(input("Type your choice: "))
+    while user_input != "A" and user_input != "B" and user_input != "a" and user_input != "b":
         print("Enter either A(a) or B(b)!")
-        userinput = str(input("ENTER: "))
+        user_input = str(input("Type your choice: "))
     else:
-        if userinput == "A" or userinput == "a":
-            return "A", redA
-        elif userinput == "B" or userinput == "b":
-            return "B", redB
+        if user_input == "A" or user_input == "a":
+            return "A", script0
+        elif user_input == "B" or user_input == "b":
+            return "B", script1
 
 
-def mkch3(redA, redB, redC):
-    userinput = str(input("ENTER: "))
-    while userinput != "A" and userinput != "B" and userinput != "C" and userinput != "a" and userinput != "b" and userinput != "c":
+def mkch3(script0, script1, script2):
+    user_input = str(input("Type your choice: "))
+    while user_input != "A" and user_input != "B" and user_input != "C" and user_input != "a" and user_input != "b" and user_input != "c":
         print("Enter either A, B or C!")
-        userinput = str(input("ENTER: "))
+        user_input = str(input("Type your choice: "))
     else:
-        if userinput == "A" or userinput == "a":
-            return "A", redA
-        elif userinput == "B" or userinput == "b":
-            return "B", redB
-        elif userinput == "C" or userinput == "c":
-            return "C", redC
+        if user_input == "A" or user_input == "a":
+            return "A", script0
+        elif user_input == "B" or user_input == "b":
+            return "B", script1
+        elif user_input == "C" or user_input == "c":
+            return "C", script2
 
 
-def mkch4(redA, redB, redC, redD):
-    userinput = str(input("ENTER: "))
-    while userinput != "A" and userinput != "B" and userinput != "C" and userinput != "D" and userinput != "a" and userinput != "b" and userinput != "c" and userinput != "d":
+def mkch4(script0, script1, script2, script3):
+    user_input = str(input("Type your choice: "))
+    while user_input != "A" and user_input != "B" and user_input != "C" and user_input != "D" and user_input != "a" and user_input != "b" and user_input != "c" and user_input != "d":
         print("Enter either A, B, C or D!")
-        userinput = str(input("ENTER: "))
+        user_input = str(input("Type your choice: "))
     else:
-        if userinput == "A" or userinput == "a":
-            return "A", redA
-        elif userinput == "B" or userinput == "b":
-            return "B", redB
-        elif userinput == "C" or userinput == "c":
-            return "C", redC
-        elif userinput == "D" or userinput == "d":
-            return "D", redD
+        if user_input == "A" or user_input == "a":
+            return "A", script0
+        elif user_input == "B" or user_input == "b":
+            return "B", script1
+        elif user_input == "C" or user_input == "c":
+            return "C", script2
+        elif user_input == "D" or user_input == "d":
+            return "D", script3
 
 
-def mkch5(redA, redB, redC, redD, redE):
-    userinput = str(input("ENTER: "))
-    while userinput != "A" and userinput != "B" and userinput != "C" and userinput != "D" and userinput != "E" and userinput != "a" and userinput != "b" and userinput != "c" and userinput != "d" and userinput != "e":
+def mkch5(script0, script1, script2, script3, script4):
+    user_input = str(input("Type your choice: "))
+    while user_input != "A" and user_input != "B" and user_input != "C" and user_input != "D" and user_input != "E" and user_input != "a" and user_input != "b" and user_input != "c" and user_input != "d" and user_input != "e":
         print("Enter either A, B, C, D or E!")
-        userinput = str(input("ENTER: "))
+        user_input = str(input("Type your choice: "))
     else:
-        if userinput == "A" or userinput == "a":
-            return "A", redA
-        elif userinput == "B" or userinput == "b":
-            return "B", redB
-        elif userinput == "C" or userinput == "c":
-            return "C", redC
-        elif userinput == "D" or userinput == "d":
-            return "D", redD
-        elif userinput == "E" or userinput == "e":
-            return "E", redE
+        if user_input == "A" or user_input == "a":
+            return "A", script0
+        elif user_input == "B" or user_input == "b":
+            return "B", script1
+        elif user_input == "C" or user_input == "c":
+            return "C", script2
+        elif user_input == "D" or user_input == "d":
+            return "D", script3
+        elif user_input == "E" or user_input == "e":
+            return "E", script4
 
 
-def mkch6(redA, redB, redC, redD, redE, redF):
-    userinput = str(input("ENTER: "))
-    while userinput != "A" and userinput != "B" and userinput != "C" and userinput != "D" and userinput != "E" and userinput != "F" and userinput != "a" and userinput != "b" and userinput != "c" and userinput != "d" and userinput != "e" and userinput != "f":
+def mkch6(script0, script1, script2, script3, script4, script5):
+    user_input = str(input("Type your choice: "))
+    while user_input != "A" and user_input != "B" and user_input != "C" and user_input != "D" and user_input != "E" and user_input != "F" and user_input != "a" and user_input != "b" and user_input != "c" and user_input != "d" and user_input != "e" and user_input != "f":
         print("Enter either A, B, C, D, E or F!")
-        userinput = str(input("ENTER: "))
+        user_input = str(input("Type your choice: "))
     else:
-        if userinput == "A" or userinput == "a":
-            return "A", redA
-        elif userinput == "B" or userinput == "b":
-            return "B", redB
-        elif userinput == "C" or userinput == "c":
-            return "C", redC
-        elif userinput == "D" or userinput == "d":
-            return "D", redD
-        elif userinput == "E" or userinput == "e":
-            return "E", redE
-        elif userinput == "F" or userinput == "f":
-            return "F", redF
+        if user_input == "A" or user_input == "a":
+            return "A", script0
+        elif user_input == "B" or user_input == "b":
+            return "B", script1
+        elif user_input == "C" or user_input == "c":
+            return "C", script2
+        elif user_input == "D" or user_input == "d":
+            return "D", script3
+        elif user_input == "E" or user_input == "e":
+            return "E", script4
+        elif user_input == "F" or user_input == "f":
+            return "F", script5
 
 
-def mkch7(redA, redB, redC, redD, redE, redF, redG):
-    userinput = str(input("ENTER: "))
-    while userinput != "A" and userinput != "B" and userinput != "C" and userinput != "D" and userinput != "E" and userinput != "F" and userinput != "G" and userinput != "a" and userinput != "b" and userinput != "c" and userinput != "d" and userinput != "e" and userinput != "f" and userinput != "g":
+def mkch7(script0, script1, script2, script3, script4, script5, script6):
+    user_input = str(input("Type your choice: "))
+    while user_input != "A" and user_input != "B" and user_input != "C" and user_input != "D" and user_input != "E" and user_input != "F" and user_input != "G" and user_input != "a" and user_input != "b" and user_input != "c" and user_input != "d" and user_input != "e" and user_input != "f" and user_input != "g":
         print("Enter either A, B, C, D, E, F or G!")
-        userinput = str(input("ENTER: "))
+        user_input = str(input("Type your choice: "))
     else:
-        if userinput == "A" or userinput == "a":
-            return "A", redA
-        elif userinput == "B" or userinput == "b":
-            return "B", redB
-        elif userinput == "C" or userinput == "c":
-            return "C", redC
-        elif userinput == "D" or userinput == "d":
-            return "D", redD
-        elif userinput == "E" or userinput == "e":
-            return "E", redE
-        elif userinput == "F" or userinput == "f":
-            return "F", redF
-        elif userinput == "G" or userinput == "g":
-            return "G", redG
+        if user_input == "A" or user_input == "a":
+            return "A", script0
+        elif user_input == "B" or user_input == "b":
+            return "B", script1
+        elif user_input == "C" or user_input == "c":
+            return "C", script2
+        elif user_input == "D" or user_input == "d":
+            return "D", script3
+        elif user_input == "E" or user_input == "e":
+            return "E", script4
+        elif user_input == "F" or user_input == "f":
+            return "F", script5
+        elif user_input == "G" or user_input == "g":
+            return "G", script6
 
 
-def mkch8(redA, redB, redC, redD, redE, redF, redG, redH):
-    userinput = str(input("ENTER: "))
-    while userinput != "A" and userinput != "B" and userinput != "C" and userinput != "D" and userinput != "E" and userinput != "F" and userinput != "G" and userinput != "H" and userinput != "a" and userinput != "b" and userinput != "c" and userinput != "d" and userinput != "e" and userinput != "f" and userinput != "g" and userinput != "h":
+def mkch8(script0, script1, script2, script3, script4, script5, script6, script7):
+    user_input = str(input("Type your choice: "))
+    while user_input != "A" and user_input != "B" and user_input != "C" and user_input != "D" and user_input != "E" and user_input != "F" and user_input != "G" and user_input != "H" and user_input != "a" and user_input != "b" and user_input != "c" and user_input != "d" and user_input != "e" and user_input != "f" and user_input != "g" and user_input != "h":
         print("Enter either A, B, C, D, E, F, G or H!")
-        userinput = str(input("ENTER: "))
+        user_input = str(input("Type your choice: "))
     else:
-        if userinput == "A" or userinput == "a":
-            return "A", redA
-        elif userinput == "B" or userinput == "b":
-            return "B", redB
-        elif userinput == "C" or userinput == "c":
-            return "C", redC
-        elif userinput == "D" or userinput == "d":
-            return "D", redD
-        elif userinput == "E" or userinput == "e":
-            return "E", redE
-        elif userinput == "F" or userinput == "f":
-            return "F", redF
-        elif userinput == "G" or userinput == "g":
-            return "G", redG
-        elif userinput == "H" or userinput == "h":
-            return "H", redH
+        if user_input == "A" or user_input == "a":
+            return "A", script0
+        elif user_input == "B" or user_input == "b":
+            return "B", script1
+        elif user_input == "C" or user_input == "c":
+            return "C", script2
+        elif user_input == "D" or user_input == "d":
+            return "D", script3
+        elif user_input == "E" or user_input == "e":
+            return "E", script4
+        elif user_input == "F" or user_input == "f":
+            return "F", script5
+        elif user_input == "G" or user_input == "g":
+            return "G", script6
+        elif user_input == "H" or user_input == "h":
+            return "H", script7
 
