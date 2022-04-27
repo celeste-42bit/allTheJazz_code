26efe659009370d8d9b34b9a796cbb176bf404d7
jasha-gruener <studi.jasha.gruener@gmail.com>
2022-04-27 19:14:00 +0200

    Added issue todo!
    Signed-off-by: jasha-gruener <studi.jasha.gruener@gmail.com>

--- scripts.py
+++ scripts.py
@@ -1,6 +1,12 @@
+from fileops import *
+from choiceeng import *
+
 def ch0br0():  # chapter 0 branch 0
     print("I am a script, \nthe script of choice A")
-
+    display("N_start.txt")
+    display("C_choice.txt")
+    # choice("ch0br0()", "ch0br1l()")
+    #TODO find out why this throws an error!
 
 def ch0br1l():  # chapter 0 branch 1 option "left"
     print("I am another script, \nthe script of choice B")
